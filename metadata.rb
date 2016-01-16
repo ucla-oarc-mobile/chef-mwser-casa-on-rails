546f37403dd3aca090f1b25c9caf9f13ff2235a2
Steve Nolen <technolengy@gmail.com>
2016-01-15 16:29:38 -0800

    bugfix conf file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures ectg-casa-on-rails'
 long_description 'Installs/Configures ectg-casa-on-rails'
-version '0.1.0'
+version '0.1.1'
 
 depends 'casa-on-rails'
 depends 'elasticsearch'
