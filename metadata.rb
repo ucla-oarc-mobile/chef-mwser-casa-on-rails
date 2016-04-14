fa89744f1f9988fb5ea36f40287645763aad883f
Joshua Selsky <jselsky@oit.ucla.edu>
2016-04-14 14:31:41 -0700

    Pin the demo CASA at master, but force the other SaaS instances (except casa-caliper) to use the official released revision from the resource.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures mwser-casa-on-rails'
 long_description 'Installs/Configures mwser-casa-on-rails'
-version '0.2.13'
+version '0.2.14'
 
 depends 'casa-on-rails'
 depends 'mysql'
