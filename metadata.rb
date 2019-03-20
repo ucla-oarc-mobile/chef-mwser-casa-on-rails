3dbee723f0d651ea8f008f352d9ee42d2e6d6743
Keith Rozett <krozett@gmail.com>
2019-03-20 14:06:01 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures mwser-casa-on-rails'
 long_description 'Installs/Configures mwser-casa-on-rails'
-version '0.2.40'
+version '0.2.41'
 
 depends 'casa-on-rails'
 depends 'mysql'
